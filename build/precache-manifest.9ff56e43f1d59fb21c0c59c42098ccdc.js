5806051fc2095975827a6906aebf5960b59c0be9
Marc Strong <marcstrong@yahoo.com>
2020-07-08 22:22:36 -0500

    template react app

---
+++ build/precache-manifest.9ff56e43f1d59fb21c0c59c42098ccdc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bec2b0023f3984b275bc6e703dc94a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8539fa520cb412b2468f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "626cf241871b4951ddfe",
+    "url": "/static/js/2.79cb504e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.79cb504e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8539fa520cb412b2468f",
+    "url": "/static/js/main.1848895a.chunk.js"
+  },
+  {
+    "revision": "9dd6a93b70f2e0c8e403",
+    "url": "/static/js/runtime-main.88672a9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

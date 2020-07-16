f3d8fa4ca0b78d12ce067573cc95398458181b5b
Marc Strong <marcstrong@yahoo.com>
2020-07-16 10:02:38 -0500

    update server.js, add seeds, models and routes

---
+++ scripts/seedDB.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+// This file empties the Books collection and inserts the books below
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/googlebooks"
+);
+
+const bookSeed = [
+  {
+    title: "The Hunger Games",
+    authors: ["Suzanne Collins"],
+    description: "First in the ground-breaking HUNGER GAMES trilogy, this new foiled edition of THE HUNGER GAMES is available for a limited period of time. Set in a dark vision of the near future, a terrifying reality TV show is taking place. Twelve boys and twelve girls are forced to appear in a live event called The Hunger Games. There is only one rule: kill or be killed. When sixteen-year-old Katniss Everdeen steps forward to take her younger sister's place in the games, she sees it as a death sentence. But Katniss has been close to death before. For her, survival is second nature.",
+    image: "http://books.google.com/books/content?id=_zSzAwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
+    link: "http://books.google.com/books?id=_zSzAwAAQBAJ&dq=hunger+games&hl=&source=gbs_api",
+  }
+];
+
+db.Book
+  .remove({})
+  .then(() => db.Book.collection.insertMany(bookSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
